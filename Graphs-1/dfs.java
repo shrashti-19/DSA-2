6826fe2d04ca0af59bf934700d2fa8daf3d7a5f7
shrashti-19 <shrashtidwivedi91@gmail.com>
2026-02-09 18:15:59 +0530

    dfs modified with connected components code

--- Graphs-1/dfs.java
+++ Graphs-1/dfs.java
@@ -12,13 +12,20 @@ public class dfs{
             //this.wt = weight;
         }
     }
-    public static void dfs(ArrayList<Edge>[] graph, int curr, boolean visited[]){
+    public static void dfs(ArrayList<Edge>[] graph, boolean[]visited){
+        for(int i=0 ; i<graph.length ; i++){
+            if(!visited[i]){
+                dfsutil(graph, i, visited);
+            }
+        }
+    }
+    public static void dfsutil(ArrayList<Edge>[] graph, int curr, boolean visited[]){
         System.out.println(curr + " ");
         visited[curr] = true;
         for(int i=0 ; i<graph[curr].size() ; i++){
             Edge e = graph[curr].get(i);
             if(!visited[e.dest]){
-                dfs(graph,e.dest, visited);
+                dfsutil(graph,e.dest, visited);
             }
         }
     }
@@ -33,10 +40,10 @@ public class dfs{
         graph[0].add(new Edge(0,2));
 
         graph[1].add(new Edge(1,0));
-        graph[1].add(new Edge(1,3));
+        
         
 
-        graph[2].add(new Edge(2,4));
+        graph[2].add(new Edge(2,3));
         
 
         graph[3].add(new Edge(3,1));
@@ -44,17 +51,16 @@ public class dfs{
         graph[3].add(new Edge(3,5));
 
 
-        graph[4].add(new Edge(4,2));
-        graph[4].add(new Edge(4,3));
         graph[4].add(new Edge(4,5));
+        
 
-        graph[5].add(new Edge(5,3));
+        
         graph[5].add(new Edge(5,4));
         graph[5].add(new Edge(5,6));
 
         graph[6].add(new Edge(6,5));
 
-        dfs(graph,0,new boolean[V]);
+        dfs(graph, new boolean[V]);
 
     }
   }
